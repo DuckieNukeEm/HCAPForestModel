5362ba3ae97dca0195765f5bef747337695c0d0a
DuckieNukeEm <DuckieNukeEm@users.noreply.github.com>
2015-11-11 15:19:49 -0600

    Create HCAPFunctions.R

---
+++ R/HCAPFunctions.R
@@ -0,0 +1,8 @@
+#######################################################################
+#This file contains the functions that will be needed for tree's
+#######################################################################
+
+#This is the print function for the verbose
+vprint <- function(...){
+  if(verbose) {print(...)}
+}
