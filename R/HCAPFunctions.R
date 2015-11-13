4b8d0f77e769debeea56d230d37306ded24d4e56
DuckieNukeEm <DuckieNukeEm@users.noreply.github.com>
2015-11-13 15:15:48 -0600

    added Create.formula function

--- R/HCAPFunctions.R
+++ R/HCAPFunctions.R
@@ -6,3 +6,9 @@
 vprint <- function(...){
   if(verbose) {print(...)}
 }
+
+create.formula <- function(Dep , indep, sep.value = "+", dep.sep.value = sep.value ){
+      dep.side <- paste(Dep,collapse = dep.sep.value)
+      indep.side <- paste(indep, collapse = sep.value)
+      return (as.formula(paste(dep.side, indep.side, sep = "~")))
+}
